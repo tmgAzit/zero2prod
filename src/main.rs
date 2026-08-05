caaf7c7b9c4b8df9c777e88a47ad02e50b0722a8
ajit <azit.ghising@gmail.com>
2026-08-06 04:10:44 +0930

    Server is runned in a default port.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main()-> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    let port = listener.local_addr().unwrap();
+
+    println!("Listening at port:{}", port);
+    run(listener).await?;
+    tokio::signal::ctrl_c().await?;
+    Ok(())
 }
