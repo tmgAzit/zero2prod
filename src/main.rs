d4e5ba8cf65b20d3f15faf10cd408633c15bcc2e
ajit <azit.ghising@gmail.com>
2026-08-06 15:08:56 +0930

    serde crates added for the tesing.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use zero2prod::run;
 use tokio::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
-async fn main()-> Result<(), std::io::Error> {
+async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     let port = listener.local_addr().unwrap();
 
