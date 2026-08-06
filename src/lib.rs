25960db667d8787eb37c4f9b8502c87c62dab419
ajit <azit.ghising@gmail.com>
2026-08-06 15:09:30 +0930

    New handler subscriber and new route subscribtions are define.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
-use axum::{http::StatusCode, Router, routing::get};
+use axum::{
+    Router,
+    extract::Form,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
-async fn health_check()-> StatusCode{
+#[derive(Debug, Serialize, Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
-pub async fn run(listener:TcpListener) -> Result<(), std::io::Error> {
-   let router = Router::new().route("/check_health", get(health_check));
-    tokio::spawn(async move { axum::serve(listener, router).await.unwrap();});
-   Ok(())
+async fn subscribe(Form(_user): Form<FormData>) -> StatusCode {
+    if _user.email.is_empty() || _user.name.is_empty() {
+        return StatusCode::BAD_REQUEST;
+    }
+    StatusCode::OK
+}
+
+pub async fn run(listener: TcpListener) -> Result<(), std::io::Error> {
+    let router = Router::new()
+        .route("/check_health", get(health_check))
+        .route("/subscriptions", post(subscribe));
+    tokio::spawn(async move {
+        axum::serve(listener, router).await.unwrap();
+    });
+    Ok(())
 }
