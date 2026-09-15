097ba6099871a425ff66565da244be7a2f026fd5
ajit <azit.ghising@gmail.com>
2026-09-16 01:36:17 +0930

    New PgPool argument passed to the run function for interior mutability easily.

--- src/startup.rs
+++ src/startup.rs
@@ -4,24 +4,18 @@ use axum::{
      };
  use tokio::net::TcpListener;
  use crate::routes::{health_check, subscribe};
- use sqlx::PgConnection;
- use std::sync::{Arc, Mutex};
+ use sqlx::PgPool;
 
-#[derive(Clone)]
-struct AppState {
-    conn: Arc<Mutex<PgConnection>>,
-}
 
-    pub async fn run(listener: TcpListener, pool: PgConnection) -> Result<(), std::io::Error> {
+ pub async fn run(listener: TcpListener, pool:PgPool) -> Result<(), std::io::Error> {
 
-        let state = AppState {
-            conn: Arc::new(Mutex::new(pool)),
-        };
        let router = Router::new()
            .route("/check_health", get(health_check))
-            .route("/subscriptions", post(subscribe)).with_state(state.conn);
+            .route("/subscriptions", post(subscribe)).with_state(pool);
        tokio::spawn(async move {
-           axum::serve(listener, router).await.unwrap();
+           if let Err(e) = axum::serve(listener, router).await {
+               eprintln!("server error: {e}");
+           }
        });
        Ok(())
    }
