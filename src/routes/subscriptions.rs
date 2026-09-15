7bdfd12b00eb98ad1a417494d24394c7446d3868
ajit <azit.ghising@gmail.com>
2026-09-16 01:37:15 +0930

    New subscriptions methods are defined and connected to the database

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
-use axum::{ extract::Form, http::StatusCode};
-use sqlx::PgConnection;
+use axum::{ extract::{Form, State }, http::StatusCode, response::{Response, IntoResponse}};
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData{
@@ -7,6 +9,31 @@ pub struct FormData{
     name: String, 
 }
 
-pub async fn subscribe(_form:Form<FormData>, _conn: PgConnection) -> StatusCode {
-    StatusCode::OK
+#[derive(Debug)]
+pub struct AppError (
+    pub sqlx::Error
+        );
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        eprintln!("Internal server error: {:?}", self.0);
+        (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+    }
+}
+
+pub async fn subscribe(State(pool): State<PgPool>, form: Form<FormData>) -> Result<StatusCode, AppError> {
+        sqlx::query!(
+         r#"
+            INSERT INTO subscriptions (id, email, name, subscribed_at) 
+            VALUES ( $1, $2, $3, $4)
+            "#,
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            Utc::now()
+         )
+        .execute(&pool)
+        .await
+        .map_err(AppError)?;
+   Ok(StatusCode::OK)
 }
